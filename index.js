bf5395e69c755fc65015fc69b64ed635d3540bd9
theworld <136832190+sandiptamangdev@users.noreply.github.com>
2025-12-08 09:55:13 +0545

    feat: refactored it to less

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const readline = require('readline');
+// can't use for loop as it is synchronous and rl.question is asynchronous.
 
-const rl = readline.createInterface({
+import { createInterface } from 'readline';
+
+const rl = createInterface({
     input: process.stdin,
     output: process.stdout
 });
@@ -12,137 +14,93 @@ console.log("The number is between 1 - 100.");
 console.log("You have 10 attempts to guess the number.");
 console.log("");
 
-const randomNum = Math.floor(Math.random() * 100);
+const randomNum = Math.floor(Math.random() * 100) + 1;
+console.log(randomNum);
+
+function checkNumber(num) {
+
+    if (num === randomNum) {
+        console.log(`You won the correct number is ${num}.`);
+        // rl.close();
+        return true;
+    } else if (num > randomNum) {
+        console.log(`Choose lower number then ${num}.`);
+    } else {
+        console.log(`Choose higher number then ${num}.`);
+    }
+    console.log("");
+    return false;
+}
 
 rl.question('Choose first number: ', (num1) => {
     const a = parseFloat(num1);
-    if (a > randomNum) {
-        console.log(`Choose lower number then ${a}.`);
-    } else if (a === randomNum) {
-        console.log(`You won the correct number is ${a}.`);
-        rl.close()
+    if(checkNumber(a)) {
+        rl.close();
         return;
-    } else {
-        console.log(`Choose higher number then ${a}.`);
     }
-    console.log("");
 
     rl.question("Choose second number: ", (num2) => {
         const b = parseFloat(num2);
-        if (b > randomNum) {
-            console.log(`Choose lower number then ${b}.`);
-        } else if (b === randomNum) {
-            console.log(`You won the correct number is ${b}.`);
-            rl.close()
-            return;
-        } else {
-            console.log(`Choose higher number then ${b}.`);
-        }
-        console.log("");
+            if(checkNumber(b)) {
+                rl.close();
+                return;
+            }
 
         rl.question("Choose third number: ", (num3) => {
             const c = parseFloat(num3);
-            if (c > randomNum) {
-                console.log(`Choose lower number then ${c}.`);
-            } else if (c === randomNum) {
-                console.log(`You won the correct number is ${c}.`);
-                rl.close()
-                return;
-            } else {
-                console.log(`Choose higher number then ${c}.`);
-            }
-            console.log("");
+                if(checkNumber(c)) {
+                    rl.close();
+                    return;
+                }
 
             rl.question("Choose fouth number: ", (num4) => {
                 const d = parseFloat(num4);
-                if (d > randomNum) {
-                    console.log(`Choose lower number then ${d}.`);
-                } else if (d === randomNum) {
-                    console.log(`You won the correct number is ${d}.`);
-                    rl.close()
-                    return;
-                } else {
-                    console.log(`Choose higher number then ${d}.`);
-                }
-                console.log("");
+                    if(checkNumber(d)) {
+                        rl.close();
+                        return;
+                    }
 
                 rl.question("Choose fifth number: ", (num5) => {
                     const e = parseFloat(num5);
-                    if (e > randomNum) {
-                        console.log(`Choose lower number then ${e}.`);
-                    } else if (e === randomNum) {
-                        console.log(`You won the correct number is ${e}.`);
-                        rl.close()
-                        return;
-                    } else {
-                        console.log(`Choose higher number then ${e}.`);
-                    }
-                    console.log("");
+                        if(checkNumber(e)) {
+                            rl.close();
+                            return;
+                        }
 
                     rl.question("Choose sixth number: ", (num6) => {
                         const f = parseFloat(num6);
-                        if (f > randomNum) {
-                            console.log(`Choose lower number then ${f}.`);
-                        } else if (f === randomNum) {
-                            console.log(`You won the correct number is ${f}.`);
-                            rl.close()
-                            return;
-                        } else {
-                            console.log(`Choose higher number then ${f}.`);
-                        }
-                        console.log("");
+                            if(checkNumber(f)) {
+                                rl.close();
+                                return;
+                            }
 
                         rl.question("Choose seventh number: ", (num7) => {
                             const g = parseFloat(num7);
-                            if (g > randomNum) {
-                                console.log(`Choose lower number then ${g}.`);
-                            } else if (g === randomNum) {
-                                console.log(`You won the correct number is ${g}.`);
-                                rl.close()
-                                return;
-                            } else {
-                                console.log(`Choose higher number then ${g}.`);
-                            }
-                            console.log("");
+                                if(checkNumber(g)) {
+                                    rl.close();
+                                    return;
+                                }
 
                             rl.question("Choose eighth number: ", (num8) => {
                                 const h = parseFloat(num8);
-                                if (h > randomNum) {
-                                    console.log(`Choose lower number then ${h}.`);
-                                } else if (h === randomNum) {
-                                    console.log(`You won the correct number is ${h}.`);
-                                    rl.close()
-                                    return;
-                                } else {
-                                    console.log(`Choose higher number then ${h}.`);
-                                }
-                                console.log("");
+                                    if(checkNumber(h)) {
+                                        rl.close();
+                                        return;
+                                    }
                             
                                 rl.question("Choose ninth number: ", (num9) => {
                                     const i = parseFloat(num9);
-                                    if (i > randomNum) {
-                                        console.log(`Choose lower number then ${i}.`);
-                                    } else if (i === randomNum) {
-                                        console.log(`You won the correct number is ${i}.`);
-                                        rl.close()
-                                        return;
-                                    } else {
-                                        console.log(`Choose higher number then ${i}.`);
-                                    }
-                                    console.log("");
+                                        if(checkNumber(i)) {
+                                            rl.close();
+                                            return;
+                                        }
 
                                     rl.question("Choose tenth number: ", (num10) => {
                                         const j = parseFloat(num10);
-                                        if (j > randomNum) {
-                                            console.log(`Choose lower number then ${j}.`);
-                                        } else if (j === randomNum) {
-                                            console.log(`You won the correct number is ${j}.`);
-                                            rl.close()
-                                            return;
-                                        } else {
-                                            console.log(`Choose higher number then ${j}.`);
-                                        }
-                                        console.log("");
+                                            if(checkNumber(j)) {
+                                                rl.close();
+                                                return;
+                                            }
 
                                         console.log("Bro your luck is bad i gave you 10 attempt");
 
@@ -159,29 +117,27 @@ rl.question('Choose first number: ', (num1) => {
 });
 
 
-//refactor in progress;
+// rl.question('Choose first number: ', (answer) => {
+//     const a = parseFloat(answer);
+//     checkNumber(a);
+// });
 
-// function checkNumber(num) {
-//     if (num > randomNum) {
-//         console.log(`Choose lower number then ${num}.`);
-//     } else if (num === randomNum) {
-//         console.log(`You won the correct number is ${num}.`);
-//         gameOver = true;
-//         rl.close()
-//         return;
-//     } else {
-//         console.log(`Choose higher number then ${num}.`);
-//     }
-//     console.log("");
-// }
+// A loop that runs 10 times
+
+// rl.question('Choose number: ', (answer) => {
+//     // This callback is executed *later* when the user responds.
+//     const a = parseFloat(answer);
+//     checkNumber(a);
+// });
+
 
 // function askQuestion() {
-//     for(let i = 0; i <= 10; i++) {
+//     // for(let i = 10; i <= 0; i--){
 //         rl.question('Choose first number: ', (answer) => {
 //             const a = parseFloat(answer);
 //             checkNumber(a);
 //         });
-//     }
+//     // }
 // }
 
 // askQuestion()
